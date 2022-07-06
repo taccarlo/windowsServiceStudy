cd94f716e21b8f8709745d3515e1b2bc0fb9168b
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-07-06 17:55:39 +0200

    [ADD] first commit

---
+++ digiseiWindowsService/digiseiWindowsService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digiseiWindowsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+#if DEBUG
+            Console.WriteLine("Hello!");
+            Service1 myService = new Service1();
+            myService.OnDebug();
+            
+            //this will keep our service alive
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+
+#endif
+        }
+    }
+}
---
+++ digiseiWindowsService/digiseiWindowsService/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digiseiWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
